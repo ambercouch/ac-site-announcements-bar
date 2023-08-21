5c7988366c2be156e90d2d999f08a9046f1a1020
Richie Arnold <richard@ambercouch.co.uk>
2023-08-21 14:20:17 +0100

    ACSAB - Update announcement bar template for mobile

--- ac-site-announcements-bar.php
+++ ac-site-announcements-bar.php
@@ -81,6 +81,7 @@ function acsab($atts){
         'showposts' => 1,
     ));
     if (have_posts()) :
+        $linkSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M448 279.196V464c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h248a24 24 0 0 1 16.97 7.029l16 16C344.09 102.149 333.382 128 312 128H64v320h320V295.196c0-6.365 2.529-12.47 7.029-16.971l16-16C422.148 247.106 448 257.814 448 279.196zM576 37.333C576 16.715 559.285 0 538.667 0H380c-15.464 0-28 12.536-28 28v17.885c0 15.766 13.011 28.424 28.772 27.989l67.203-1.906L199.09 319.09c-9.429 9.363-9.457 24.605-.061 34.001l23.879 23.879c9.396 9.396 24.639 9.369 34.001-.06l247.122-248.885-1.906 67.203c-.434 15.76 12.224 28.772 27.99 28.772H548c15.464 0 28-12.536 28-28V37.333z"/></svg>';
 
         $output .= '<style>';
         $output .= 'body{margin-top:32px;}';
@@ -93,18 +94,27 @@ function acsab($atts){
         while (have_posts()):
             the_post();
     $announcement = get_field('announcement_text');
+    $announcementMob = get_field('announcement_text_mobile');
     $linkUrl = get_field('announcement_link_url');
     $linkText = get_field('announcement_link_text');
     $announcementBtn = "<span class='c-btn--announcement' >";
     $announcementBtn .= "<a href='".$linkUrl."'>" . $linkText . "</a>";
     $announcementBtn .= "</span>";
+
+    $announcementMobHtml = "<a href='".$linkUrl."'>";
+    $announcementMobHtml .= $announcementMob.$linkSvg;
+    $announcementMobHtml .= "</a>";
+
             ob_start();
             ?>
 
          <div class="c-acsab-announcement-bar ">
-          <div class="c-acsab-announcement-bar__content  animated backInRight">
+          <div class="c-acsab-announcement-bar__content  u-d-b--wide u-d-b--desk u-d-b--tablet u-d-n--mobile u-d-n--tiny animated backInRight">
              <?php echo wpautop($announcement . ' ' . $announcementBtn) ?>
           </div>
+           <div class="c-acsab-announcement-bar__content--mobile u-d-n--wide u-d-n--desk u-d-n--tablet u-d-b--mobile u-d-b--tiny animated backInRight">
+               <?php echo wpautop($announcementMobHtml) ?>
+           </div>
          </div>
 
 <?php
